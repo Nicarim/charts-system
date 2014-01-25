f56fcedd81df723fa0b99677fda72c069bb2a113
Marcin <marcin19962@onet.eu>
2014-01-25 10:22:27 +0100

    Added UsersController
    Seperated user related actions from chart related actions

---
+++ app/controllers/UsersController.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marcin
+ * Date: 25.01.14
+ * Time: 10:19
+ */
+
+class UsersController extends BaseController{
+    public function Login(){
+        $user = array(
+            'username' => Input::get('username'),
+            'password' => Input::get('password')
+        );
+        if (Auth::attempt($user))
+            return Redirect::route('home');
+
+        return Redirect::route('login')->with('error','Your username/password was wrong, are you sure you have account?');
+
+    }
+    public function AddUser(){
+        $data = array(
+            'username' => Input::get('username'),
+            'password' => Hash::make(Input::get('password')),
+            'team' => Input::get('team')
+        );
+        $validator = Validator::make($data, array('username' => 'unique:users,username'));
+        if($validator->fails()){
+            return Redirect::to('/users/add')->with('error', $data['username'].' already exists!');
+        }
+        $user = User::create($data);
+        return Redirect::to('/users/add')->with('error', $user->username.' added!');
+    }
+    public function ListUsers(){
+        return View::make('layouts/manage-listusers')->with('users', User::withTrashed()->get());
+    }
+    public function DeleteOrRestoreUser($id, $type){
+        if($type == "restore"){
+            $user = User::withTrashed()->where("id",$id);
+            $user->restore();
+        }
+        else if ($type == "delete"){
+            $user = User::find($id);
+            $user->delete();
+        }
+        else
+            return Redirect::to('/users');
+        return Redirect::to('/users');
+    }
+    public function ChangePass(){
+        $data = array(
+            "password" => Input::get("password"),
+            "confirm_password" => Input::get("confirm_password")
+        );
+        if ($data['password'] == $data['confirm_password']){
+            $user = Auth::user();
+            $user->password = Hash::make($data['password']);
+            $user->save();
+            return Redirect::to('/users/pass')->with('error', 'Password changed!');
+        }else
+            return Redirect::to('/users/pass')->with('error','First password is different than second');
+    }
+} 
--- app/routes.php
+++ app/routes.php
@@ -9,9 +9,9 @@ Route::group(array('before' => 'access'), function(){ //admin level access
     Route::get('/users/add', function(){
         return View::make('layouts/manage-adduser');
     });
-    Route::post('/users/add', 'ChartsController@AddUser');
-    Route::get('/users', 'ChartsController@ListUsers');
-    Route::get('/users/delete/{id}/{type}', 'ChartsController@DeleteOrRestoreUser');
+    Route::post('/users/add', 'UsersController@AddUser');
+    Route::get('/users', 'UsersController@ListUsers');
+    Route::get('/users/delete/{id}/{type}', 'UsersController@DeleteOrRestoreUser');
     Route::get('/charts/add', function(){
         return View::make('layouts/charts-add');
     });
@@ -21,14 +21,14 @@ Route::group(array('before' => 'guest'), function(){ //only guests
     Route::get("/login", array ("as" => "login", function(){
         return View::make('layouts/manage-login');
     }));
-    Route::post("/login", 'ChartsController@Login');
+    Route::post("/login", 'UsersController@Login');
 });
 
 Route::group(array('before' => 'auth'), function(){ //logged in users
     Route::get('/users/pass', function(){
         return View::make('layouts/manage-password');
     });
-    Route::post('/users/pass', 'ChartsController@ChangePass');
+    Route::post('/users/pass', 'UsersController@ChangePass');
     Route::get("/charts/view/{id}/{mode?}", 'ChartsController@ViewVoting');
     Route::get("/logout", function(){
         Auth::logout();
