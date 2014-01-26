e89156a4f25758a7c1c23acbad640c2a7683dfc1
Marcin <marcin19962@onet.eu>
2014-01-26 21:01:04 +0100

    fixed name of log in button

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -35,7 +35,7 @@
             </li>
             <li><a href="/logout">Logout</a></li>
             @else
-            <li><button type="button" onclick="window.location.href='/login'" class="btn btn-default navbar-btn">Sign in</button></li>
+            <li><button type="button" onclick="window.location.href='/login'" class="btn btn-default navbar-btn">Log in</button></li>
             @endif
         </ul>
     </div>
