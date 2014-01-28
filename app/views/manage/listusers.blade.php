f00b86f44de8dcf1ae97d49fe079e87f7da925bd
marcin19962 <marcin19962@onet.eu>
2014-01-28 08:54:39 +0100

    Added buttons for changing user allowed modes.

--- app/views/manage/listusers.blade.php
+++ app/views/manage/listusers.blade.php
@@ -4,6 +4,7 @@
     <tr>
         <th style="width:40px">#</th>
         <th style="width:65%">Username</th>
+		<th></th>
         <th>Team</th>
         <th>Last Active</th>
         <th></th>
@@ -12,6 +13,14 @@
     <tr>
         <td>{{$user->id}}</td>
         <td>{{$user->username}}</td>
+		<td>
+		<div class="btn-group">
+			<button type="button" onclick="window.location.href='/users/group/{{$user->id}}/osu'" class="btn-sm {{$user->allowedMode('osu') ? 'btn-success' : 'btn-danger'}}">osu!</button> 
+			<button type="button" onclick="window.location.href='/users/group/{{$user->id}}/taiko'" class="btn-sm {{$user->allowedMode('taiko') ? 'btn-success' : 'btn-danger'}}">Taiko</button> 
+			<button type="button" onclick="window.location.href='/users/group/{{$user->id}}/ctb'" class="btn-sm {{$user->allowedMode('ctb') ? 'btn-success' : 'btn-danger'}}">CtB</button> 
+			<button type="button" onclick="window.location.href='/users/group/{{$user->id}}/mania'" class="btn-sm {{$user->allowedMode('mania') ? 'btn-success' : 'btn-danger'}}">o!m</button> 
+		</div>
+		</td>
         <td><b>{{strtoupper($user->team)}}</b></td>
         <td>{{$user->updated_at}}</td>
         @if ($user->trashed())
