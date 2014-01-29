fe77b121676c3733428cf81d7f45678dd72a61eb
Marcin <marcin19962@onet.eu>
2014-01-29 10:34:21 +0100

    Fixed iteration on users list.

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -63,7 +63,7 @@ class UsersController extends BaseController {
                     'users' => User::withTrashed()->get(),
                     'banned' => $banned
                 ));
-        }elseif($banned){
+        }elseif(!$banned){
             return View::make('manage/listusers')->with(array(
                     'users' => User::all(),
                     'banned' => $banned
