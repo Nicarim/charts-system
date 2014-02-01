c932970d60ef1e4c6b97f54e870e52596253ff91
Marcin <marcin19962@onet.eu>
2014-02-01 14:32:31 +0100

    Tried to fix beatmap results list.

--- app/models/Beatmap.php
+++ app/models/Beatmap.php
@@ -10,7 +10,7 @@ class Beatmap extends Eloquent {
 
     protected $table = "beatmaps";
     protected $fillable = array("id","artist","title","creator","osumode","ctbmode","taikomode","maniamode","chart_id");
-    public function votes($mode){
-        return $this->hasMany('vote')->where('gamemode','=',$mode);
+    public function votes(){
+        return $this->hasMany('vote');
     }
 } 
