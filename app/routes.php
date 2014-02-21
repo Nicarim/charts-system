4ea15a523bb5c0b5d54357df8ec4bfc9d46222ad
Marcin <marcin19962@onet.eu>
2014-02-21 15:25:02 +0100

    Fixed wrong route.

--- app/routes.php
+++ app/routes.php
@@ -65,7 +65,7 @@ Route::group(
         Route::get('/charts/vote/add/{beatmap}/{chart}/{mode}', 'ChartsController@addVote');
         Route::get('/charts/vote/remove/{vote}','ChartsController@removeVote');
         Route::post('/users/pass', 'UsersController@ChangePass');
-        Route::get("/charts/view/{id}/{mode?}/{csv?}", 'ChartsController@ViewVoting');
+        Route::get("/charts/view/{id}/{mode?}", 'ChartsController@ViewVoting');
         Route::get(
             "/logout",
             function () {
@@ -74,6 +74,6 @@ Route::group(
                 return Redirect::route("home");
             }
         );
-        Route::get('/charts/results/{id}/{mode?}','ChartsController@ViewResults');
+        Route::get('/charts/results/{id}/{mode?}/{csv?}','ChartsController@ViewResults');
     }
 );
