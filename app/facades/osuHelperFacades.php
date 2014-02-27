f8e9fd163a8c5beddde74c1da2f4f2dabefd669c
Marcin <marcin19962@onet.eu>
2014-02-27 19:23:45 +0100

    Add osuHelperFacade

---
+++ app/facades/osuHelperFacades.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marcin
+ * Date: 27.02.14
+ * Time: 18:12
+ */
+
+use Illuminate\Support\Facades\Facade;
+//random comment
+class osuHelperFacade extends Facade{
+    protected static function getFacadeAccessor() { return 'osuHelper'; }
+//asd
+} 
