2fa2a103f1731ae9e7cf793a52708cbb11aa1d87
Marcin <marcin19962@onet.eu>
2014-02-28 19:31:24 +0100

    Now panels will be opened by default.

--- public/js/main.js
+++ public/js/main.js
@@ -47,3 +47,20 @@ function checkIfFull(){
         });
     }
 }
+$(function(){
+    var hash = window.location.hash;
+    hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+    $('.nav-tabs a').click(function (e) {
+        $(this).tab('show');
+        var scrollmem = $('body').scrollTop();
+        window.location.hash = this.hash;
+        $('html,body').scrollTop(scrollmem);
+    });
+});
+
+$(document).ready(function() {
+    var anchor = window.location.hash.replace("#", "");
+    $(".collapse").collapse('hide');
+    $("#" + anchor).collapse('show');
+});
