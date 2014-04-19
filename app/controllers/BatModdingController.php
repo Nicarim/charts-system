0efaa60db4df863b6ce354b6ec12568725f6b3e4
Marcin <marcin19962@onet.eu>
2014-04-19 18:15:34 +0200

    Fix NEXT typo

--- app/controllers/BatModdingController.php
+++ app/controllers/BatModdingController.php
@@ -12,7 +12,7 @@ class BatModdingController extends BaseController{
         $data = Input::all();
         $modpost = new BatMod;
         $modpost['topic_id'] = $data['topic_id'];
-        $modpost['subforum'] = $data['subforum_id'];
+        $modpost['subforum_id'] = $data['subforum_id'];
         $modpost['icon_id'] = $data['icon_id'];
         $modpost['user_id'] = $data['user_id'];
         $modpost->save();
