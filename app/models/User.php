7ba5c2b824baee87cc3a2a6010cbae49d7d1c9c2
Marcin <marcin19962@onet.eu>
2014-06-18 13:42:27 +0200

    Add functions for laravel upgrade.

--- app/models/User.php
+++ app/models/User.php
@@ -93,6 +93,20 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
      *
      * @return string
      */
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
     public function getReminderEmail() {
         return $this->email;
     }
